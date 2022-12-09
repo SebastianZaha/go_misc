f7f01206f28024a7ddea754928c3e6f1440b4f55
Sebastian Zaha <sebastian.zaha@gmail.com>
2022-12-09 10:37:03 +0100

    optimize to skip some checks

--- aoc2022/day9/main.go
+++ aoc2022/day9/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var visited map[int]bool = map[int]bool{}
@@ -18,7 +19,7 @@ D 1
 L 5
 R 2`
 
-func moveSingleLink(direction byte, is []int, js []int, head int, tail int) {
+func moveSingleLink(direction byte, is []int, js []int, head int, tail int) bool {
 	diffi := is[head] - is[tail]
 	diffj := js[head] - js[tail]
 
@@ -36,12 +37,15 @@ func moveSingleLink(direction byte, is []int, js []int, head int, tail int) {
 		if adiffj > 0 {
 			js[tail] += int(diffj / adiffj) // +-1
 		}
+		return true
 	} else if adiffj == 2 {
 		js[tail] += int(diffj / adiffj)
 		if adiffi > 0 {
 			is[tail] += int(diffi / adiffi)
 		}
+		return true
 	}
+	return false
 
 	//fmt.Printf("after move %c, is[head] %d, js[head] %d, is[tail] %d, js[tail] %d\n", direction, is[head], js[head], is[tail], js[tail])
 }
@@ -59,14 +63,11 @@ func move(direction byte, is []int, js []int) {
 	}
 
 	for i := 0; i < len(is)-1; i++ {
-		moveSingleLink(direction, is, js, i, i+1)
+		if !moveSingleLink(direction, is, js, i, i+1) {
+			return
+		}
 	}
-	fmt.Printf("%+v\n%+v\n\n", is, js)
-	visit(is[len(is)-1], js[len(is)-1])
-}
-
-func visit(i, j int) {
-	visited[i*100000+j] = true
+	visited[1000*is[len(is)-1]+js[len(is)-1]] = true
 }
 
 func main() {
@@ -74,8 +75,12 @@ func main() {
 	// js := []int{1000, 1000}
 	is := []int{1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000}
 	js := []int{1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000}
+	visited[1000*100000+1000] = true
 
+	t := time.Now()
 	f, _ := os.ReadFile("input.txt")
+	fmt.Printf("file read in %v\n", time.Since(t))
+	t = time.Now()
 	for _, l := range strings.Split(string(f), "\n") {
 		if len(l) == 0 {
 			continue
@@ -85,6 +90,5 @@ func main() {
 			move(l[0], is, js)
 		}
 	}
-	fmt.Println(len(visited))
-
+	fmt.Printf("Part 2: %d (in %v)\n", len(visited), time.Since(t))
 }
