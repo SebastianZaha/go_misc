2859df8d409c8f215572e48df40196dc03296c4c
Sebastian Zaha <sebastian.zaha@gmail.com>
2022-12-10 07:35:20 +0100

    part2 sort of

--- aoc2022/day10/main.go
+++ aoc2022/day10/main.go
@@ -5,52 +5,56 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
-
-	"golang.org/x/exp/slices"
 )
 
 func main() {
-	t := time.Now()
 	f, _ := os.ReadFile("input.txt")
-
 	var (
-		cycles      int64
-		reg         int64 = 1
-		sum         int64
-		interesting = []int64{20, 60, 100, 140, 180, 220}
+		cycles int64
+		reg    int64 = 1
+		screen       = ""
 	)
 
 	for _, l := range strings.Split(string(f), "\n") {
 		if len(l) == 0 {
 			continue
 		}
-		fmt.Printf("%d %s\n", cycles, l)
-
 		if l == "noop" {
 			cycles += 1
 
-			if idx := slices.Index(interesting, cycles); idx > 0 {
-				val := interesting[idx] * reg
-				sum += val
-				fmt.Printf("%d, %d, %d, %d, %d\n", interesting[idx], cycles, reg, val, sum)
+			check := (cycles - 1) % 40
+			if check == 0 {
+				screen += "\n"
 			}
-			continue
-		}
-		num, _ := strconv.ParseInt(l[5:], 10, 64)
-		reg += num
-		cycles += 2
 
-		if idx := slices.Index(interesting, cycles-1); idx >= 0 {
-			val := interesting[idx] * (reg - num)
-			sum += val
-			fmt.Printf("%d, %d, %d, %d, %d, %d\n", interesting[idx], cycles, reg, num, val, sum)
-		} else if idx := slices.Index(interesting, cycles); idx >= 0 {
-			val := interesting[idx] * (reg - num)
-			sum += val
-			fmt.Printf("%d, %d, %d, %d, %d\n", interesting[idx], cycles, reg, val, sum)
+			if reg-1 == check || reg == check || reg+1 == check {
+				screen += "#"
+			} else {
+				screen += "."
+			}
+		} else {
+			num, _ := strconv.ParseInt(l[5:], 10, 64)
+			reg += num
+			cycles += 2
+			check := (cycles - 2) % 40
+			check1 := (cycles - 1) % 40
+			if check1 == 0 {
+				screen += "\n"
+			}
+			if reg-num-1 == check || reg-num == check || reg-num+1 == check {
+				screen += "#"
+			} else {
+				screen += "."
+			}
+			if check1 == 0 {
+				screen += "\n"
+			}
+			if reg-num-1 == check1 || reg-num == check1 || reg-num+1 == check1 {
+				screen += "#"
+			} else {
+				screen += "."
+			}
 		}
 	}
-
-	fmt.Printf("Day10 1: %d. reg: %d, total cycles %d (in %v)\n", sum, reg, cycles, time.Since(t))
+	fmt.Println(screen)
 }
